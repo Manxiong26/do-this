0371bf062d747e5a1874f64af4dcec4782fab8a4
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-26 15:27:27 -0500

    update task saga to allow fetching of task and was able to get the task generator to generate random task

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-    let sqlText = `SELECT * FROM task;`;
-    
+    // let sqlText = `SELECT * FROM task;`;
+    let sqlText = `SELECT *
+    FROM  (
+       SELECT DISTINCT 1 + trunc(random() * 7)::integer AS id
+       FROM   generate_series(1, 7) g
+       ) r
+    JOIN   task USING (id)
+    LIMIT  1;`;
     pool.query(sqlText)
     .then((result) => {
         console.log(sqlText);
--- src/components/TaskGenerator/TaskGenerator.jsx
+++ src/components/TaskGenerator/TaskGenerator.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 function TaskGenerator(){
 
-    const history = useHistory();
+    // const history = useHistory();
+    // const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     dispatch({ type: 'FETCH_TASK' });
+    // }, []);
+
+    // const task = useSelector((store) => store.task)
 
     const task = [
         "go for a walk at Phalen Lake", 
@@ -12,7 +20,7 @@ function TaskGenerator(){
         'Do Some Jumping Jacks'
     ];
 
-    const [random, setRandom] = useState(0)
+    const [random, setRandom] = useState('')
 
 const randomNumber = () => {
     const random = Math.floor(Math.random() * task.length);
@@ -24,7 +32,7 @@ const handleAccept = () => {
     return(
         <>
             <form onSubmit={handleAccept}>
-                <h1>Random Task </h1><p>{task[random]}</p>
+                <h1>Random Task </h1>{task[random]}
                 <button type="submit">Accept</button>
             </form>
             <div>
