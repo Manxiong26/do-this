e5d5fef3241cc3aaafc777751a2a97704007df7e
Maneena Xiong <xiong.maneena@gmail.com>
2021-05-26 01:03:31 -0500

    created a TaskList component

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,16 @@
+
+
+function TaskList(){
+    return (
+        <>
+        <h1>TaskList</h1>
+        <ul>
+            <li>
+                task
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default TaskList;
