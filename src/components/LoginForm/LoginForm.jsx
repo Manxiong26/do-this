8c0aba8b8da803371265d2cff4cafbe154aba13a
Maneena Xiong <xiong.maneena@gmail.com>
2021-08-18 15:59:34 -0500

    deleted some code

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -71,42 +71,6 @@ function LoginForm() {
   const classes = useStyles();
   return (
     <>
-      {/* <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form> */}
-
       <Grid container component="main" className={classes.root} onSubmit={login} className="Grid">
         <CssBaseline />
         {/* <Grid item xs={false} sm={4} md={7} className={classes.image} /> */}
