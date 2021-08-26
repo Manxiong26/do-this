bcdfce3e0fcc8a74901a4d0b0aec2b492188ba9e
Maneena Xiong <xiong.maneena@gmail.com>
2021-08-25 23:00:12 -0500

    updated copyright

--- src/components/TaskGenerator/TaskGenerator.jsx
+++ src/components/TaskGenerator/TaskGenerator.jsx
@@ -1,37 +1,26 @@
 import React, { useEffect } from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
+
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, Link, useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import {
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Avatar,
   Grid,
-  Box,
   Button,
-  Divider,
   Typography,
-  IconButton,
+  Box,
 } from "@material-ui/core";
 import { useStyles } from "../classes";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" color="secondary" align="center">
       {'Copyright © '}
-        Maneena Xiong
+      Maneena Xiong
       {' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
-
-
-
 
 function TaskGenerator() {
 
@@ -65,52 +54,9 @@ function TaskGenerator() {
   const classes = useStyles();
   return (
     <>
-      {/* <Grid container component="main" className={classes.root} className="Grid">
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <div className={classes.paper} key={task.id}>
-            <Typography component="h1" variant="h5">
-              Welcome, {user.username}! <br />
-            Today's Task
-          </Typography>
-          <img  className="img" src={task.task_img}></img>
-            <Typography component="h1" variant="h5">
-              {task.name_task}:<br /> {task.task_description}
-            </Typography>
-            <Typography component="h1" variant="h5">
-              Address is:<br /> {task.location_id}
-            </Typography>
-            <form className={classes.form} noValidate onSubmit={handleAccept}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="outlined"
-                color="primary"
-                className={classes.submit}
-              >
-                Accept
-            </Button>
-              <Button
-                fullWidth
-                variant="outlined"
-                color="primary"
-                className={classes.submit}
-                onClick={handleTask}
-              >
-                Reject
-            </Button>
-              <Box mt={5}>
-                <Copyright />
-              </Box>
-            </form>
-          </div>
-        </Grid>
-      </Grid> */}
-
-
       <Typography variant="h4" className={classes.welcome}>
-      Welcome, {user.username}
-    </Typography>
+        Welcome, {user.username}
+      </Typography>
       <Grid container direction="column">
         <Grid item xs={12} sm={12} lg={12} >
           <div className={classes.pageMargin}>
@@ -121,20 +67,20 @@ function TaskGenerator() {
               <img className={classes.image} src={task.task_img}></img>
             </p>
             <Typography variant="body2" className={classes.imageInfo}>
-              <b>{task.task_description}</b>  
+              <b>{task.task_description}</b>
             </Typography>
             <Typography variant="body2" className={classes.imageInfo}>
               Address: {task.location_id}
             </Typography>
             <form className={classes.center} onSubmit={handleAccept}>
-            <Button 
-            type="submit"
-            variant="outlined"
-            color="secondary"
-            className={classes.submit}
-            >
-              Accept
-            </Button>       
+              <Button
+                type="submit"
+                variant="outlined"
+                color="secondary"
+                className={classes.submit}
+              >
+                Accept
+              </Button>
               <Button
                 variant="outlined"
                 color="secondary"
@@ -142,14 +88,14 @@ function TaskGenerator() {
                 onClick={handleTask}
               >
                 Reject
-            </Button>
+              </Button>
             </form>
-        </div>
+          </div>
         </Grid>
-      </Grid>  
-
-
-
+      </Grid>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
     </>
   )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -19,7 +19,7 @@ function Copyright() {
     <Typography variant="body2" color="secondary" align="center">
       {'Copyright © '}
       {' '}
-        Maneena Xiong
+      Maneena Xiong
       {' '}
       {new Date().getFullYear()}
       {'.'}
@@ -49,48 +49,48 @@ function TaskList() {
 
   return (
     <>
-<Typography variant="h4" className={classes.title}>
-                My Adventures
-            </Typography>
-    <Grid container spacing={2} >
-      <Grid container item xs={12} spacing={3}>  
-      <Box display="flex" flexWrap="wrap" justifyContent="center">
-        {tasks.map(task =>
-          <Card className={classes.card} key={task.id} >
-            <CardActionArea onClick={(event) => viewNote(event, task)}>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {task.name_task} 
-                  <img className={classes.image} src={task.task_img}></img>
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {task.task_description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" variant="outlined" color="primary" onClick={() =>
-                dispatch({ type: "DELETE_TASK", payload: task.id })}>
-                Delete
-        </Button>
-              {task.completed === null &&
-                <Button size="small" variant="outlined" color="primary" onClick={() =>
-                  dispatch({ type: 'UPDATE_COMPLETE', payload: task.id })}>
-                  Complete
-        </Button>}
-              {task.notes === null &&
-                <Button size="small" variant="outlined" color="primary" onClick={(event) => viewNote(event, task)}>
-                  Add Notes
-        </Button>}
-            </CardActions>
-          </Card>
-        )}
-        </Box>
+      <Typography variant="h4" className={classes.title}>
+        My Adventures
+      </Typography>
+      <Grid container spacing={2} >
+        <Grid container item xs={12} spacing={3}>
+          <Box display="flex" flexWrap="wrap" justifyContent="center">
+            {tasks.map(task =>
+              <Card className={classes.card} key={task.id} >
+                <CardActionArea onClick={(event) => viewNote(event, task)}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {task.name_task}
+                      <img className={classes.image} src={task.task_img}></img>
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {task.task_description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" variant="outlined" color="primary" onClick={() =>
+                    dispatch({ type: "DELETE_TASK", payload: task.id })}>
+                    Delete
+                  </Button>
+                  {task.completed === null &&
+                    <Button size="small" variant="outlined" color="primary" onClick={() =>
+                      dispatch({ type: 'UPDATE_COMPLETE', payload: task.id })}>
+                      Complete
+                    </Button>}
+                  {task.notes === null &&
+                    <Button size="small" variant="outlined" color="primary" onClick={(event) => viewNote(event, task)}>
+                      Add Notes
+                    </Button>}
+                </CardActions>
+              </Card>
+            )}
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
-    <Box mt={5}>
-          <Copyright />
-        </Box>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
     </>
   )
 }
